25c8144d3024192acbd326ee3f132f1d931de409
paylolomar <paylolomar@gmail.com>
2016-11-15 20:26:02 -0600

    arreglando lo que arruine
    sip fui yo lo siento

--- Administracion/Secretaria/inserciones.php
+++ Administracion/Secretaria/inserciones.php
@@ -17,18 +17,18 @@
     <title>Inserciones</title>
 
     <!-- inicio de todas las importaciones de la plantilla -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
- 	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/proyecto/css/bootstrap.min.css" rel="stylesheet">
+ 	<link href="/proyecto/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-	<link href="css/general.css" rel="stylesheet">
-	<link href="css/custom.css" rel="stylesheet">
-	<link href="css/owl.carousel.css" rel="stylesheet">
-    <link href="css/owl.theme.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link href="css/animate.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/magnific-popup.css"> 
-	<script src="js/modernizr-2.8.3.min.js"></script>  
+	<link href="/proyecto/css/general.css" rel="stylesheet">
+	<link href="/proyecto/css/custom.css" rel="stylesheet">
+	<link href="/proyecto/css/owl.carousel.css" rel="stylesheet">
+    <link href="/proyecto/css/owl.theme.css" rel="stylesheet">
+	<link href="/proyecto/css/style.css" rel="stylesheet">
+	<link href="/proyecto/css/animate.css" rel="stylesheet">
+	<link rel="stylesheet" href="/proyecto/css/magnific-popup.css"> 
+	<script src="/proyecto/js/modernizr-2.8.3.min.js"></script>  
 	<!-- Modernizr /-->
 	<!--[if IE 9]>
 		<script src="js/PIE_IE9.js"></script>
@@ -459,11 +459,11 @@
 	<!--fin de las ventanas modales-->
 
     <!-- JavaScript -->
-    <script src="js/jquery-1.10.2.js"></script>
-    <script src="js/bootstrap.js"></script>
-	<script src="js/owl.carousel.js"></script>
-	<script src="js/script.js"></script>
-	<script src="js/stickUp.min.js"></script>
+    <script src="/proyecto/js/jquery-1.10.2.js"></script>
+    <script src="/proyecto/js/bootstrap.js"></script>
+	<script src="/proyecto/js/owl.carousel.js"></script>
+	<script src="/proyecto/js/script.js"></script>
+	<script src="/proyecto/js/stickUp.min.js"></script>
 	<script type="text/javascript">
 	  jQuery(function($) {
 		$(document).ready( function() {
@@ -473,14 +473,14 @@
 	  });
 	
 	</script>
-	<script type="text/javascript" src="js/jquery.corner.js"></script> 
-	<script src="js/wow.min.js"></script>
+	<script type="text/javascript" src="/proyecto/js/jquery.corner.js"></script> 
+	<script src="/proyecto/js/wow.min.js"></script>
 	<script>
 		new WOW().init();
 	</script>
-	<script src="js/classie.js"></script>
-	<script src="js/uiMorphingButton_inflow.js"></script>
-	<script src="js/jquery.magnific-popup.js"></script> 
+	<script src="/proyecto/js/classie.js"></script>
+	<script src="/proyecto/js/uiMorphingButton_inflow.js"></script>
+	<script src="/proyecto/js/jquery.magnific-popup.js"></script>
 
 </body>
 
